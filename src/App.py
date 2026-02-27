b9a085d2281889320fa863160f5a952f6408ea89
alhhITU <alhh@itu.dk>
2026-02-27 20:11:32 +0100

    Made functionality for a "back" button which redirects to the previous visited page.
    This was done through keeping track of pages via a stack.

--- src/App.py
+++ src/App.py
@@ -1,16 +1,21 @@
 import sys
+from tkinter.font import Font
 from PySide6.QtWidgets import *
-from PySide6.QtCore import QTimer
+from PySide6.QtCore import QTimer, Qt
+from PySide6.QtGui import QFont, QPalette
 class MainWindow(QMainWindow):
     def __init__ (self):
         super().__init__() 
         self.resize(900,600)
         self.setWindowTitle("App")
+        self.windowTracker = []
+        self.setStyleSheet('background-color: blue')
+        
 
         
         self.AllWindows = QStackedWidget() #Basically a container that keeps all the windows (widgets) for the application
         mainPageLayout = QHBoxLayout() #The layout for this page MainWindow.
-        mainPage = QWidget() #The page in itself.
+        self.mainPage = QWidget() #The page in itself.
 
         #Definition of different pages for the application:
         self.filePage = FilePage()
@@ -18,47 +23,73 @@ class MainWindow(QMainWindow):
 
         #Definition of buttons that will redirect.
         fileButton = QPushButton("Go to files")
+        fileButton.setStyleSheet('background-color: grey')
         createButton = QPushButton("Start creating")
+        createButton.setStyleSheet('background-color: grey')
+        self.backButton = QPushButton("Go Back")
+        self.backButton.setStyleSheet('background-color: grey')
+
         createButton.setFixedSize(100,100)
-        createButton.setCheckable(True)
         fileButton.setFixedSize(100,100)
-        fileButton.setCheckable(True)
+        self.backButton.setFixedSize(80,50)
 
         #Connects the buttons the method that handles redirection
         fileButton.clicked.connect(self.goToFilePage)
         createButton.clicked.connect(self.goToCreationPage)
+        self.backButton.clicked.connect(self.goBack)
         
         #Add the buttons to the layout
         mainPageLayout.addWidget(createButton)
         mainPageLayout.addWidget(fileButton)
+        
 
         #Put the page layout inside the mainpage.
-        mainPage.setLayout(mainPageLayout)
+        self.mainPage.setLayout(mainPageLayout)
+
 
         #Now add all the pages into the container for all the windows in the application.
-        self.AllWindows.addWidget(mainPage)
+        self.AllWindows.addWidget(self.mainPage)
         self.AllWindows.addWidget(self.filePage)
         self.AllWindows.addWidget(self.creationPage)
 
         #Center the container, and make the current page the mainpage.
+        self.setMenuWidget(self.backButton)
+        self.backButton.hide()
         self.setCentralWidget(self.AllWindows)
-        self.AllWindows.setCurrentWidget(mainPage)
+        self.AllWindows.setCurrentWidget(self.mainPage)
+
 
     def goToFilePage(self):
+        self.backButton.show()
+        self.windowTracker.append(self.mainPage)
         self.AllWindows.setCurrentWidget(self.filePage)
 
     def goToCreationPage(self):
+        self.backButton.show()
+        self.windowTracker.append(self.mainPage)
         self.AllWindows.setCurrentWidget(self.creationPage)
-        
+    
+    def goBack(self):
+        currentWindow = self.windowTracker.pop()
+        if(len(self.windowTracker) == 0):
+            self.backButton.hide()
+        self.AllWindows.setCurrentWidget(currentWindow)
 
 class FilePage(QWidget):
     def __init__ (self):
         super().__init__()
+        
         label = QLabel()
         label.setText("Welcome to the file page")
+        font = QFont('', 24)
+        label.setFont(font)
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        
+        
         layout = QVBoxLayout()
         layout.addWidget(label)
         self.setLayout(layout)
+
 
         
 
@@ -68,6 +99,9 @@ class CreatePage(QWidget):
         super().__init__()
         label = QLabel()
         label.setText("Welcome to the Creation page")
+        font = QFont('', 24)
+        label.setFont(font)
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         layout = QVBoxLayout()
         layout.addWidget(label)
         self.setLayout(layout)
