e1b065e37b7b732460d2f719de54a38cd51acc09
alhhITU <alhh@itu.dk>
2026-02-27 17:10:24 +0100

    Short documentation for the MainWindow

--- src/App.py
+++ src/App.py
@@ -7,12 +7,16 @@ class MainWindow(QMainWindow):
         self.resize(900,600)
         self.setWindowTitle("App")
 
-        self.AllWindows = QStackedWidget()
-        mainPageLayout = QHBoxLayout()
-        mainPage = QWidget()
+        
+        self.AllWindows = QStackedWidget() #Basically a container that keeps all the windows (widgets) for the application
+        mainPageLayout = QHBoxLayout() #The layout for this page MainWindow.
+        mainPage = QWidget() #The page in itself.
+
+        #Definition of different pages for the application:
         self.filePage = FilePage()
         self.creationPage = CreatePage()
 
+        #Definition of buttons that will redirect.
         fileButton = QPushButton("Go to files")
         createButton = QPushButton("Start creating")
         createButton.setFixedSize(100,100)
@@ -20,20 +24,25 @@ class MainWindow(QMainWindow):
         fileButton.setFixedSize(100,100)
         fileButton.setCheckable(True)
 
+        #Connects the buttons the method that handles redirection
         fileButton.clicked.connect(self.goToFilePage)
         createButton.clicked.connect(self.goToCreationPage)
         
+        #Add the buttons to the layout
         mainPageLayout.addWidget(createButton)
         mainPageLayout.addWidget(fileButton)
 
+        #Put the page layout inside the mainpage.
         mainPage.setLayout(mainPageLayout)
 
+        #Now add all the pages into the container for all the windows in the application.
         self.AllWindows.addWidget(mainPage)
         self.AllWindows.addWidget(self.filePage)
         self.AllWindows.addWidget(self.creationPage)
 
-        self.AllWindows.setCurrentWidget(mainPage)
+        #Center the container, and make the current page the mainpage.
         self.setCentralWidget(self.AllWindows)
+        self.AllWindows.setCurrentWidget(mainPage)
 
     def goToFilePage(self):
         self.AllWindows.setCurrentWidget(self.filePage)
