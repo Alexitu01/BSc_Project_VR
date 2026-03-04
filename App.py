ec816081f267e48d030b904e543814d96562757b
alhhITU <alhh@itu.dk>
2026-03-04 21:27:19 +0100

    Implemented simple api for the LLM, and connected the response to the frontend.
    Users can now write their descriptions, and receive a rewritten version, which improves on descriptive features in a way that is more understandable for a generative ai.
    
    (Currently no limitations with tokens set)

--- App.py
+++ App.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+    
+)
 
 app = FastAPI()
 app.mount("/FrontEnd", StaticFiles(directory="FrontEnd"), name="FrontEnd")
@@ -22,7 +30,17 @@ async def read_item_via_request_body(request: Request):
     promptForAi = await request.json()
     data = promptForAi["text"]
     print(data)
-    return data
+    SystemData = "You need to optimize the following prompt, making it specifically easy for a generative ai to understand and perform better. Your response should only contain the rewritten prompt: "
+
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": SystemData},
+        {"role": "user", "content": data}
+    ],
+    model="llama-3.3-70b-versatile",
+    )
+
+    return chat_completion.choices[0].message.content
 
 
 
